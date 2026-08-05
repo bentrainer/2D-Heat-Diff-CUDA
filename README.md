bb2cfbba2ce9bf02bc0e309d8a2b0c9d5d978d0f
bentrainer <117783189+bentrainer@users.noreply.github.com>
2026-08-05 14:22:50 -0500

    docs: update the problem definition

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # 2D-Heat-Diff-CUDA
+
 ELC 5319 MPP Final Project
+
+## Problem Definition
+
+$$
+\frac{\partial T}{\partial t} = \alpha \left( \frac{\partial^2 T}{\partial x^2} + \frac{\partial^2 T}{\partial y^2} \right)
+$$
+
+In a discretized form, using explicit Euler's method:
+
+$$
+\frac{T_{n+1}(i,j) - T_{n}(i,j)}{\Delta t} = \alpha \left[
+    \frac{T_{n}(i+1,j) - 2T_{n}(i,j) + T_{n}(i-1,j)}{\Delta x^2} +
+    \frac{T_{n}(i,j+1) - 2T_{n}(i,j) + T_{n}(i,j-1)}{\Delta y^2}
+\right]
+$$
+
+Tidy up to get
+
+$$
+\begin{align*}
+T_{n+1}(i,j) = \left( 1 - \frac{2\Delta t}{\Delta x^2} - \frac{2\Delta t}{\Delta y^2} \right) &T_{n}(i,j) \\
++ \frac{\Delta t}{\Delta x^2} &T_{n}(i-1,j) \\
++ \frac{\Delta t}{\Delta x^2} &T_{n}(i+1,j) \\
++ \frac{\Delta t}{\Delta y^2} &T_{n}(i,j+1) \\
++ \frac{\Delta t}{\Delta y^2} &T_{n}(i,j-1)
+\end{align*}
+$$
+
+### Parameters
+
+- Width $L_x$, Height $L_y$
+
+- Sample number in $x$, $y$ direction: $N_x$, $N_y$
+
+- Mesh size $\Delta x = \frac{L_x}{N_x-1}$, $\Delta y = \frac{L_y}{N_y-1}$
+
+- Time step $n$, step size $\Delta t$
+
+- $\alpha=1.0$
+
+## TODO
+
+- [ ] naive single-threaded CPU for-loop baseline
+
+- [ ] CUDA naive global-memory implementation
+
+- [ ] CUDA shared-memory tiled implementation
+
+- [ ] CUDA thread-coarsened implementation
+
+### Optional comparison
+
+- [ ] oneMKL sparse-matrix implementation
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include <cassert>
+
 #include "CLI11.hpp"
+
 
 int main(int argc, char** argv) {
     CLI::App app{"2D Heat Diffusion Test"};
@@ -9,9 +12,23 @@ int main(int argc, char** argv) {
         CLI::IsMember({"cpu", "cuda-naive"})
     );
 
-    unsigned width = 1000, height = 1000;
-    app.add_option("-w,--width", width, "problem width size");
-    app.add_option("-H,--height", height, "problem height size");
+    float width = 1.0f, height = 1.0f;
+    std::size_t Nx = 1024, Ny = 1024, steps = 10;
+    app.add_option("-W,--width", width, "problem width Lx");
+    app.add_option("-H,--height", height, "problem height Ly");
+    app.add_option("-x,--Nx", Nx, "problem discretization Nx");
+    app.add_option("-y,--Ny", Ny, "problem discretization Ny");
+    app.add_option("-s,--steps", steps, "solver steps");
+
+    float alpha = 1.0f, delta_t = 1e-6;
+    app.add_option("--alpha", alpha, "alpha");
+    app.add_option("--delta-t", delta_t, "delta t");
+
+    std::size_t block_size = 16;
+    app.add_option("--block", block_size, "cuda block size");
+
+    bool verify = false;
+    app.add_flag("--verify", verify, "compare cuda result vs cpu");
 
     try {
         app.parse(argc, argv);
@@ -19,8 +36,21 @@ int main(int argc, char** argv) {
         return app.exit(e);
     }
 
+    const float delta_x = width / (1.0f * (Nx-1));
+    const float delta_y = height / (1.0f * (Ny-1));
+
+    if (alpha * delta_t * (1/(delta_x*delta_x) + 1/(delta_y*delta_y)) - 0.5f > 0) {
+        delta_t = 0.5f / (alpha * (1/(delta_x*delta_x) + 1/(delta_y*delta_y))) / 2.0f;
+        std::cout << "[WARN] update delta_t to " << delta_t << " for numerical stability\n";
+    }
+
     std::cout << "using backend: " << backend << "\n"
-              << "problem size:  " << width  << "×" << height << "\n";
+              << "problem size:  " << width  << "x" << height << "\n"
+              << "mesh size:     " << Nx << "x" << Ny << "\n"
+              << "block size:    " << block_size << "\n"
+              << "solve setup:   " << "delta_x=" << delta_x << ", delta_y=" << delta_y << "\n"
+              << "               " << "alpha=" << alpha << ", delta_t=" << delta_t << ", steps=" << steps << "\n"
+              << "verify?        " << (verify ? "YES" : "NO") << "\n";
 
     return 0;
 }
